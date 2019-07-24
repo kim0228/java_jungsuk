17727333e7bc8459c00a3ba58b21c80bf0201c7b
kim0228 <rlatjdfla309@naver.com>
2019-07-24 11:39:39 +0900

    ..

--- java-workspace/ch03/src/OperatorEx15.java
+++
@@ -1,8 +0,0 @@
-﻿class OperatorEx15 { 
-      public static void main(String[] args) { 
-            char lowerCase = 'a'; 
-            char upperCase = (char)(lowerCase - 32); 
-
-            System.out.println(upperCase); 
-      } 
-} 
--- java-workspace/ch15/src/StandardIOEx2.java
+++
@@ -1,6 +0,0 @@
-﻿class StandardIOEx2 {
-	public static void main(String[] args) {
-		System.out.println("out : Hello World!");
-		System.err.println("err : Hello World!");
-	}
-}
