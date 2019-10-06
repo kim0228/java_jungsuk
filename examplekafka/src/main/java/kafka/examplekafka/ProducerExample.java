bcfbc7d417731f92afe10c376f8a82bb07e02e92
kim0228 <rlatjdfla309@naver.com>
2019-10-06 23:07:12 +0900

    ..

--- examplekafka/src/main/java/kafka/examplekafka/ProducerExample.java
+++ examplekafka/src/main/java/kafka/examplekafka/ProducerExample.java
@@ -1,55 +1,47 @@
 package kafka.examplekafka;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
 
-import java.util.Iterator;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ProducerExample {
-
+	public static String result;
+	public static String finalResult;
 	public static void main(String[] args) throws IOException {
-		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpGet httpget = new HttpGet("https://sports.news.naver.com/wfootball/record/index.nhn?category=epl&tab=team");
+		String header;
 		try {
-			httpClient.execute(httpget, new BasicResponseHandler() {
-				@Override
-				public String handleResponse(HttpResponse response) throws HttpResponseException, IOException {
-					// 웹페이지를 그냥 갖어오면 한글이 깨져요. 인코딩 처리를 해야해요.
-					String res = new String(super.handleResponse(response).getBytes("8859_1"), "euc-kr");
-					Document doc = Jsoup.parse(res);
-					Elements rows = doc.select(".record .record_tbl");
-//					String[] items = new String[] { "순위", "팀", "경기수", "승", "무", "패", "득점", "실점", "득실차" };
-//					for (Element row : rows) {
-//						Iterator<Element> iterElem = row.getElementsByTag("td").iterator();
-//						StringBuilder builder = new StringBuilder();
-//						for (String item : items) {
-//							builder.append(item + ": " + iterElem.next().text() + "   \t");
-//						}
-//						System.out.println(builder.toString());
-//					}
-					System.out.println(rows);
-					return res;
+			Document doc = (Document)Jsoup.connect("https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_USDKRW").get();
+			Elements tableElements = doc.select("table");
+			Elements tableHeaderEles = tableElements.select("thead tr th");
+			System.out.println("headers");
+			for (int i = 0; i < tableHeaderEles.size(); i++) {
+				//  System.out.println(tableHeaderEles.get(i).text());
+				header  = tableHeaderEles.get(i).text();
 			}
-				
-			});
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			System.out.println();
+			Elements tableRowElements = tableElements.select(":not(thead) tr");
+			
+			for (int i = 0; i < tableRowElements.size(); i++) {
+				Element row = tableRowElements.get(i);
+				// System.out.println(row);
+				Elements rowItems = row.select("td");
+				for (int j = 0; j < rowItems.size(); j++) {
+					System.out.println(rowItems.get(j).text());
+					result = rowItems.get(j).text();
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-
+		///////////////////////////////////////////// kafka /////////////////////////////////////////////
 //		Properties configs = new Properties();
 //		configs.put("bootstrap.servers", "192.168.0.30:9092,192.168.0.31:9092,192.168.0.32:9092"); // kafka host 및
 //		configs.put("acks", "all"); // 자신이 보낸 메시지에 대해 카프카로부터 확인을 기다리지 않습니다.
@@ -60,18 +52,13 @@ public class ProducerExample {
 //		// producer 생성
 //		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
 //
-//		Scanner sc = new Scanner(System.in);
-//		while (true) {
-//			System.out.print("kafka 전달 메시지 입력 :");
-//			String i = sc.nextLine();
-//			producer.send(new ProducerRecord<String, String>("search", i));
-//
-//			if (i.equals("exit")) {
-//				break;
-//			}
+//		System.out.print("kafka 전달 메시지 입력 :");
+//		for(int i = 0; i<=5;i++) {
+//			finalResult = result;
+//			producer.send(new ProducerRecord<String, String>("ds-topic", finalResult));
 //		}
 //		producer.flush();
 //		producer.close();
-
+		}
 	}
-}
+
